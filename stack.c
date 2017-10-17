60ea178a4703f87f5d1844523abad3bef6b416aa
Thomas Lively <tlively123@gmail.com>
2017-10-17 15:08:16 -0400

    implement stack

--- stack.c
+++ stack.c
@@ -1,36 +1,83 @@
 #include <stdlib.h>
+#include <assert.h>
 
 #include "stack.h"
 
+typedef struct node
+{
+    item data;
+    struct node *next;
+} node;
+
 struct stack
 {
-    // TODO: implement me
+    node *head;
 };
 
 stack *stack_create()
 {
-    // TODO: implement me
-    return NULL;
+    stack *s = malloc(sizeof(stack));
+    if (s == NULL)
+    {
+        return s;
+    }
+    s->head = NULL;
+    return s;
 }
 
 bool stack_push(stack *s, item it)
 {
-    // TODO: implement me
-    (void)s;
-    (void)it;
-    return false;
+    assert(s != NULL);
+
+    // allocate new node
+    node *new_node = malloc(sizeof(node));
+    if (new_node == NULL)
+    {
+        return false;
+    }
+
+    // initialize and push node
+    new_node->data = it;
+    new_node->next = s->head;
+    s->head = new_node;
+    return true;
 }
 
 bool stack_pop(stack *s, item *out)
 {
-    // TODO: implement me
-    (void)s;
-    (void)out;
-    return false;
+    assert(s != NULL);
+    assert(out != NULL);
+
+    // check whether stack is empty
+    if (s->head == NULL)
+    {
+        return false;
+    }
+
+    // get first node and its item
+    node *old_head = s->head;
+    s->head = old_head;
+    item it = old_head->data;
+
+    free(old_head);
+    return it;
+}
+
+// recursively frees a link list
+void free_node(node *n)
+{
+    if (n == NULL)
+    {
+        return;
+    }
+
+    free_node(n->next);
+    free(n);
 }
 
 void stack_free(stack *s)
 {
-    // TODO: implement me
-    (void)s;
+    assert(s != NULL);
+    free_node(s->head);
+    free(s);
 }
